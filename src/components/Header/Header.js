718828f54c4f1d6d7b6d5c172842ef5e4afa4331
Justin <justin@justinzhao.net>
2022-01-29 16:35:41 -0500

    small visual changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -34,9 +34,24 @@ function Header() {
           </Grid>
           {/* Bio */}
           <Grid item xs={12} sm={6}>
-            <Box>
-              <Typography variant="h4">👋 Hello</Typography>
+            {/* Greeting */}
+            <Box sx={{ pl: { xs: 0, sm: 3} }}>
+              <Typography variant="h4">
+                {" "}Hello
+              </Typography>
               <Typography variant="h3">I'm Justin</Typography>
+              {/* Facts */}
+              <Box sx={{ py: { xs: 2, sm: 4}}} >
+                <Typography variant="p" component="p">
+                  NYU Stern School of Business
+                </Typography>
+                <Typography variant="p" component="p">
+                  c/o 2024
+                </Typography>
+                <Typography variant="p" component="p">
+                  southern california
+                </Typography>
+              </Box>
             </Box>
           </Grid>
         </Grid>
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,34 @@
-import { createMuiTheme } from "@mui/material";
+import { createTheme } from "@mui/material";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   typography: {
     h1: {
-      fontFamily: '"Montserrat", Open Sans',
+      fontFamily: "Roboto",
+    },
+    h2: {
+      fontFamily: "Roboto",
+    },
+    h3: {
+      fontFamily: "Roboto",
+    },
+    h4: {
+      fontFamily: "Roboto",
+    },
+    p: {
+      fontFamily: "Roboto Mono",
+    },
+  },
+  components: {
+    // Name of the component
+    MuiCard: {
+      styleOverrides: {
+        // Name of the slot
+        root: {
+          // Some CSS
+          borderRadius: "15px",
+          boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+        },
+      },
     },
   },
 });
